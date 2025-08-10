7f9da1d2ee678fff4035c1df9bfd621d0ae27f79
Wei Cheng <40693861+yesweican@users.noreply.github.com>
2025-08-09 23:43:30 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Layout from './Components/Layout';
+
+function App() {
+  return (
+      <Layout>
+        {/* Your app content goes here */}
+
+      </Layout>
+  );
+}
+
+export default App;
